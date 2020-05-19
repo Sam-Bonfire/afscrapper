963305324deee5dba37fa08a9bb7c1b025f7d2d5
Sam-Bonfire <pprathamesh98@gmail.com>
2020-05-19 14:53:51 +0530

    Testing New Changes for Code Readability

--- afscraper/views.py
+++ afscraper/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from bs4 import BeautifulSoup
 from . import models
 
-BASE_URL_AMAZON = 'https://www.amazon.in/s?k='
 BASE_URL_FLIPKART = 'https://www.flipkart.com/search?q='
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64;     x64; rv:66.0) Gecko/20100101 Firefox/66.0",
            "Accept-Encoding": "gzip, deflate",
@@ -12,6 +11,14 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64;     x64; rv:66.0)
            "Connection": "close", "Upgrade-Insecure-Requests": "1"
            }
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
+
+AMAZON = {
+    'BASE_SEARCH_URL': 'https://www.amazon.in/s?k=',
+    'BASE_PRODUCT_URL': 'https://www.amazon.in',
+    'PRODUCT_LISTING_CLASS': 'sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28',
+    'PRODUCT_TITLE_CLASS': 'a-size-medium a-color-base a-text-normal',
+    'PRODUCT_LINK_CLASS': 'a-link-normal a-text-normal',
+}
 
 
 # Create your views here.
@@ -22,14 +29,13 @@ def homepage(request):
 def new_search(request):
     search = request.POST.get("search-term")
     models.Search.objects.create(search=search)
-    response_amazon = requests.get(BASE_URL_AMAZON + requote_uri(search), headers=headers)
+    response_amazon = requests.get(AMAZON['BASE_PRODUCT_URL'] + requote_uri(search), headers=headers)
     amazon_soup = BeautifulSoup(response_amazon.content, features='html.parser')
-    final_posting = []
-    for item in amazon_soup.findAll('div', attrs={
-        'class': 'sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28'}):
-        name = item.find('span', attrs={'class': 'a-size-medium a-color-base a-text-normal'})
+    posting = []
+    for item in amazon_soup.findAll('div', attrs={'class': AMAZON['PRODUCT_LISTING_CLASS']}):
+        name = item.find('span', attrs={'class': AMAZON['PRODUCT_TITLE_CLASS']})
         image_link = None
-        link = item.find('a', attrs={'class': 'a-link-normal a-text-normal'})
+        link = item.find('a', attrs={'class': AMAZON['PRODUCT_LINK_CLASS']})
         price = item.find('span', attrs={'class': 'a-offscreen'})
 
         product = []
@@ -46,14 +52,14 @@ def new_search(request):
                 'https://www.chillinoodle.co.uk/skin/frontend/chillinoodle/default/images/catalog/product/placeholder/image.jpg')
 
         if link is not None:  # product.2
-            product.append('https://amazon.in/'+link['href'])
+            product.append(AMAZON['BASE_PRODUCT_URL'] + link['href'])
         else:
-            product.append('amazon.in')
+            product.append(AMAZON['BASE_PRODUCT_URL'])
 
         if price is not None:  # product.3
             product.append(price.text)
         else:
             product.append('NA')
-        final_posting.append(product)
-    frontend_package = {'search_term': search, 'final_posting': final_posting[1:]}
+        posting.append(product)
+    frontend_package = {'search_term': search, 'final_posting': posting[1:]}
     return render(request, 'afscraper/new_search.html', frontend_package)
